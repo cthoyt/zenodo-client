602d9cdc39dc09b6deea8fae604857f4117c7180
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-01-13 19:32:48 +0100

    Update project config

--- src/zenodo_client/__init__.py
+++ src/zenodo_client/__init__.py
@@ -2,5 +2,12 @@
 
 """A wrapper for the Zenodo API."""
 
-from .api import Zenodo, create_zenodo, ensure_zenodo, update_zenodo  # noqa:F401
+from .api import (  # noqa:F401
+    Zenodo,
+    create_zenodo,
+    download_zenodo,
+    download_zenodo_latest,
+    ensure_zenodo,
+    update_zenodo,
+)
 from .struct import Creator, Metadata  # noqa:F401
