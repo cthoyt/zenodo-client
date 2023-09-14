aa49d4bd2c6a87483221993e23bc2cdf9beae1b6
Daniel Nüst <daniel.nuest@tu-dresden.de>
2023-09-14 12:08:16 +0200

    merge two tests, reduce diff

--- tests/test_lifecycle.py
+++ tests/test_lifecycle.py
@@ -55,6 +55,12 @@ class TestLifecycle(unittest.TestCase):
             upload_type="dataset",
             description="test description",
             creators=[CREATOR],
+            access_right="embargoed",
+            language="ger",
+            version="ver1",
+            license="my-own-license",
+            publication_type="patent",
+            # image_type="figure",
             communities=[Community(identifier="zenodo"), Community(identifier="bioinformatics")],
             keywords=["key1", "key2", "key3"],
             notes="this is important",
@@ -65,9 +71,16 @@ class TestLifecycle(unittest.TestCase):
         # print(f"\n\nSEE V1 ON ZENODO: {res_json['links']['record_html']}")
         self.assertEqual(True, res_json["submitted"])
         self.assertEqual("done", res_json["state"])
-        self.assertEqual("dataset", res_json["metadata"]["upload_type"])
+        
+        self.assertEqual(data.upload_type, res_json["metadata"]["upload_type"])
         self.assertEqual(data.title, res_json["metadata"]["title"])
+        self.assertEqual(data.description, res_json["metadata"]["description"])
+        self.assertEqual(len(data.creators), len(res_json["metadata"]["creators"]))
+        self.assertEqual(data.access_right, res_json["metadata"]["access_right"])
+        self.assertEqual(data.language, res_json["metadata"]["language"])
         self.assertEqual(data.version, res_json["metadata"]["version"])
+        self.assertEqual(data.license, res_json["metadata"]["license"])
+        self.assertEqual(data.publication_type, res_json["metadata"]["publication_type"])
         self.assertEqual({"zenodo", "bioinformatics"}, {c["identifier"] for c in res_json["metadata"]["communities"]})
         self.assertEqual(data.keywords, res_json["metadata"]["keywords"])
         self.assertEqual(data.notes, res_json["metadata"]["notes"])
@@ -86,41 +99,6 @@ class TestLifecycle(unittest.TestCase):
         res_json = res.json()
         # print(f"SEE V3 ON ZENODO: {res_json['links']['record_html']}")
         self.assertEqual(f"{data.version}-2", res_json["metadata"]["version"])
-
-    def test_metadata_fields(self):
-        """Test metadata fields."""
-        data = Metadata(
-            title="Test Upload",
-            upload_type="publication",
-            description="test description",
-            creators=[CREATOR],
-            access_right="embargoed",
-            language="ger",
-            version="ver1",
-            license="my-own-license",
-            publication_type="patent",
-            # image_type="figure",
-            communities=["zenodo", "bioinformatics"],
-            keywords=["key1", "key2", "key3"],
-            notes="this is important",
-        )
-
-        res = self.zenodo.ensure(key=self.key, data=data, paths=self.path)
-        res_json = res.json()
-
-        self.assertEqual(data.title, res_json["metadata"]["title"])
-        self.assertEqual(data.upload_type, res_json["metadata"]["upload_type"])
-        self.assertEqual(data.description, res_json["metadata"]["description"])
-        self.assertEqual(len(data.creators), len(res_json["metadata"]["creators"]))
-        self.assertEqual(data.access_right, res_json["metadata"]["access_right"])
-        self.assertEqual(data.language, res_json["metadata"]["language"])
-        self.assertEqual(data.version, res_json["metadata"]["version"])
-        self.assertEqual(data.license, res_json["metadata"]["license"])
-        self.assertEqual(data.publication_type, res_json["metadata"]["publication_type"])
-        self.assertEqual(len(data.communities), len(res_json["metadata"]["communities"]))
-        self.assertEqual(data.communities[1], res_json["metadata"]["communities"][1]["id"])
-        self.assertEqual(data.keywords, res_json["metadata"]["keywords"])
-        self.assertEqual(data.notes, res_json["metadata"]["notes"])
 
     def test_create_no_orcid(self):
         """Test create with no ORCID."""
