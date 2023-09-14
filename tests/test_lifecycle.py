6780b59f98864a51b9bc502d7e61a9b3a11de89d
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-09-14 08:49:48 +0200

    Update test_lifecycle.py

--- tests/test_lifecycle.py
+++ tests/test_lifecycle.py
@@ -36,6 +36,8 @@ class TestLifecycle(unittest.TestCase):
     def setUp(self) -> None:
         """Set up the test case with a zenodo client."""
         self.zenodo = Zenodo(sandbox=True)
+        self.assertIsInstance(self.zenodo.access_token, str)
+        self.assertNotEqual("", self.zenodo.access_token)
         self.key = f"test-{uuid4()}"
         self._directory = tempfile.TemporaryDirectory()
         self.directory = Path(self._directory.name).resolve()
